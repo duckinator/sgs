ecb73e372ea3761523c50704b7a2c110b91598da
Ellen Marie Dash <me@duckie.co>
2023-10-25 21:39:04 -0400

    UI looks way better.

--- src/main.rs
+++ src/main.rs
@@ -32,68 +32,101 @@ impl eframe::App for App {
         let inner_margin = egui::style::Margin::same(10.0);
         let frame = egui::containers::Frame::central_panel(&ctx.style()).inner_margin(inner_margin);
 
-        egui::TopBottomPanel::top("panel").frame(frame).show(ctx, |ui| {
-            egui::Grid::new("panel-grid").show(ui, |ui| {
-                if ui.button("Speak").clicked() {
-                    if let Err(error) = self.panel.speak(&mut self.speech_engine) {
-                        panic!("{:?}", error);
-                    }
-                }
+        let button_size = [80.0, 80.0];
 
-                ui.horizontal(|ui| {
-                    for entry in &self.panel.entries {
-                        // It doesn't make sense to use a button here,
-                        // I just want each phrase to be distinguishable.
-                        //
-                        // For now, assign it to nothing.
-                        // Long term, probably use a label with a background.
-                        let _ = ui.button(entry.label.clone());
+        egui::CentralPanel::default().frame(frame).show(ctx, |ui| {
+            egui::Grid::new("main-grid").show(ui, |ui| {
+                // With each [] being a sub-grid:
+                // | [Speak]           | [Panel] | [Delete, Clear] |
+                // -------------------------------------------------
+                // | [Layout selector] | [system] | [extra]        |
+
+                // Row 1, Column 1
+                egui::Grid::new("top-left").show(ui, |ui| {
+                    let egui_button = egui::Button::new("Speak");
+                    if ui.add_sized(button_size, egui_button).clicked() {
+                        if let Err(error) = self.panel.speak(&mut self.speech_engine) {
+                            panic!("{:?}", error);
+                        }
                     }
                 });
 
-                if ui.button("Delete").clicked() {
-                    self.panel.remove_last_entry();
-                }
+                // Row 1, Column 2
+                egui::Grid::new("top-center").show(ui, |ui| {
+                    let cols = 9.0;
+                    let inner_spacing = ui.ctx().style().spacing.item_spacing[0];
+                    let max_width = cols * (button_size[0] + inner_spacing);
 
-                if ui.button("Clear").clicked() {
-                    self.panel.clear();
-                }
-            });
-        });
+                    ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP).with_main_wrap(true), |ui| {
+                        ui.set_max_width(max_width);
 
-        egui::SidePanel::left("layout-selector").frame(frame).show(ctx, |ui| {
-            egui::Grid::new("layout-selector-grid").show(ui, |ui| {
-                for (idx, layout) in self.board.layouts.iter().enumerate() {
-                    let button = egui::Button::new(layout.name.clone()).selected(self.current_layout == idx);
-                    if ui.add(button).clicked() {
-                        self.current_layout = idx;
-                    }
-                    ui.end_row();
-                }
-            });
-        });
-
-        egui::CentralPanel::default().frame(frame).show(ctx, |ui| {
-            egui::Grid::new("board").show(ui, |ui| {
-                let layout = &self.board.layouts[self.current_layout];
-                for row in 0..layout.rows {
-                    for col in 0..layout.cols {
-                        if let Some(button) = layout.get_button(col, row) {
-                            if ui.button(button.label.clone()).clicked() {
-                                if layout.immediate {
-                                    self.speech_engine.speak(button.get_pronouncible_text()).expect("Failed to speak word");
-                                } else {
-                                    self.panel.add_entry(button);
-                                }
-                            }
-                        } else {
-                            // No button for (row, col).
+                        for entry in &self.panel.entries {
+                            // It doesn't make sense to use a button here,
+                            // I just want each phrase to be distinguishable.
+                            //
+                            // For now, assign it to nothing.
+                            // Long term, probably use a label with a background.
+                            let egui_button = egui::Button::new(entry.label.clone());
+                            let _ = ui.add_sized(button_size, egui_button);
                         }
-                    }
-                    ui.end_row();
-                }
+                    });
+                });
 
-                //Ok(self.board.clone());
+                // Row 1, Column 3
+                egui::Grid::new("top-right").show(ui, |ui| {
+                    if ui.button("Delete").clicked() {
+                        self.panel.remove_last_entry();
+                    }
+
+                    if ui.button("Clear").clicked() {
+                        self.panel.clear();
+                    }
+                });
+
+                ui.end_row();
+
+                // Row 2, Column 1
+                egui::Grid::new("layout-selector-grid").show(ui, |ui| {
+                    for (idx, layout) in self.board.layouts.iter().enumerate() {
+                        let egui_button = egui::Button::new(layout.name.clone()).selected(self.current_layout == idx);
+                        if ui.add_sized(button_size, egui_button).clicked() {
+                            self.current_layout = idx;
+                        }
+                        ui.end_row();
+                    }
+                });
+
+                // Row 2, Column 2
+                egui::Grid::new("board").show(ui, |ui| {
+                    let layout = &self.board.layouts[self.current_layout];
+                    for row in 0..layout.rows {
+                        for col in 0..layout.cols {
+                            if let Some(button) = layout.get_button(col, row) {
+                                let egui_button = egui::Button::new(button.label.clone());
+                                if ui.add_sized(button_size, egui_button).clicked() {
+                                    if layout.immediate {
+                                        self.speech_engine.speak(button.get_pronouncible_text()).expect("Failed to speak word");
+                                    } else {
+                                        self.panel.add_entry(button);
+                                    }
+                                }
+                            } else {
+                                // No button for (row, col).
+                                //let egui_label = egui::Label::new("");
+                                let egui_label = egui::Button::new("");
+                                ui.add_sized(button_size, egui_label);
+                            }
+                        }
+                        ui.end_row();
+                    }
+
+                    //Ok(self.board.clone());
+                });
+
+                // Row 3, Column 3
+                egui::Grid::new("bottom-right").show(ui, |ui| {
+                    // ...
+                });
             });
         });
     }
