891ca67e01d61cf5fae3c3669ad86556cc538562
Ellen Marie Dash <me@duckie.co>
2024-03-01 22:04:50 -0500

    Add support for wasm builds.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
+pub mod app;
 pub mod system;
 pub mod button;
 pub mod panel;
 pub mod speech;
+
+#[cfg(target_arch = "wasm32")]
+mod wasm;
+
+#[cfg(target_arch = "wasm32")]
+pub use wasm::WebHandle;
