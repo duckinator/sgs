7c9377bbf616ddd8eca2828caca0104ef52466d0
Ellen Marie Dash <me@duckie.co>
2024-03-05 16:28:05 -0500

    have test-site.sh actually build the code

--- test-site.sh
+++ test-site.sh
@@ -2,6 +2,8 @@
 
 rm -rf _site || exit $?
 cp -r ./web ./_site || exit $?
+
+cargo build --target wasm32-unknown-unknown || exit $?
 wasm-bindgen --target web --out-dir _site/ target/wasm32-unknown-unknown/debug/sgs.wasm || exit $?
 
 cd _site
